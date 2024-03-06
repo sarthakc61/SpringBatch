726e804e6d1a00c87b32bb73c0050c9a182821fc
sarthakc61 <48508711+sarthakc61@users.noreply.github.com>
2024-03-06 12:48:10 +0530

    Spring Batch Processing

---
+++ src/main/java/com/spring/batch/config/CustomerProcessor.java
@@ -0,0 +1,19 @@
+package com.spring.batch.config;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.spring.batch.entity.Customer;
+
+public class CustomerProcessor implements ItemProcessor<Customer, Customer>{
+
+	@Override
+	public Customer process(Customer customer) throws Exception {
+//		if(customer.getCountry().equals("United States")) {
+//            return customer;
+//        }else{
+//            return null;
+//        }
+		return customer;
+	}
+
+}
---
+++ src/main/java/com/spring/batch/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.spring.batch.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.spring.batch.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+}
